bf46a9625293dba8c12cbfe95233a64aeb5a84a6
Phammai <lmai8861@gmail.com>
2025-05-29 11:47:14 +0700

    Add files via upload

---
+++ GUI/GUI/FormMDI.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class FormMDI : Form
+    {
+        private string tenDangNhap;
+
+        public FormMDI(string tenDangNhap)
+        {
+            InitializeComponent();
+            this.tenDangNhap = tenDangNhap;
+        }
+
+        private void FormMDI_Load(object sender, EventArgs e)
+        {
+            lblWelcome.Text = "Xin chào, " + tenDangNhap + "!";
+        }
+
+        public FormMDI()
+        {
+            InitializeComponent();
+        }
+        private void lblWelcome_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void FormMDI_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            Formtrangchu F = new Formtrangchu();
+            F.MdiParent = this;
+        }
+    }
+}
