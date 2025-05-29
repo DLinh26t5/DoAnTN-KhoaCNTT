cc68d24714e44a01f1f092c4f1791601f46a35a3
Phammai <lmai8861@gmail.com>
2025-05-29 11:06:22 +0700

    Add files via upload

---
+++ GUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            FormDangNhap FormDangNhap = new FormDangNhap();
+
+            if (FormDangNhap.ShowDialog() == DialogResult.OK)
+            {
+                Application.Run(new FormMDI(FormDangNhap.tenDangNhap));
+            }
+        }
+    } }
