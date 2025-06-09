1ad604575a928eba806a59754ed36a6f73d6ec2d
Phammai <lmai8861@gmail.com>
2025-06-10 01:14:34 +0700

    Add files via upload

---
+++ GUI/FormLanThi.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace ĐOAN_QLBD
+{
+    partial class FormLanThi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonThem = new System.Windows.Forms.Button();
+            this.buttonSua = new System.Windows.Forms.Button();
+            this.buttonXoa = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.comboBoxTenKhoa = new System.Windows.Forms.ComboBox();
+            this.comboBoxTenNganh = new System.Windows.Forms.ComboBox();
+            this.comboBoxHocKy = new System.Windows.Forms.ComboBox();
+            this.comboBoxHinhThuc = new System.Windows.Forms.ComboBox();
+            this.comboBoxMonHoc = new System.Windows.Forms.ComboBox();
+            this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 6;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label8, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 2, 5);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.dateTimePicker, 4, 3);
+            this.tableLayoutPanel1.Controls.Add(this.comboBoxTenKhoa, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.comboBoxTenNganh, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.comboBoxHocKy, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.comboBoxHinhThuc, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.comboBoxMonHoc, 4, 1);
+            this.tableLayoutPanel1.Controls.Add(this.numericUpDown, 4, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(874, 546);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.label1, 4);
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(46, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(780, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lần thi";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label2.Location = new System.Drawing.Point(49, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 44);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên khóa:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label3.Location = new System.Drawing.Point(85, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 44);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tên ngành:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label4.Location = new System.Drawing.Point(73, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 44);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Học kỳ:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label5.Location = new System.Drawing.Point(47, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 44);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Hình thức";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label8.Location = new System.Drawing.Point(452, 132);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(112, 44);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Ngày thi:";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label7.Location = new System.Drawing.Point(466, 88);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 44);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Lần thi:";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label6.Location = new System.Drawing.Point(445, 44);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(119, 44);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Môn học:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel1.SetColumnSpan(this.tableLayoutPanel2, 3);
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.Controls.Add(this.buttonThem, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonSua, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonXoa, 2, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(177, 223);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(649, 38);
+            this.tableLayoutPanel2.TabIndex = 8;
+            // 
+            // buttonThem
+            // 
+            this.buttonThem.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonThem.Location = new System.Drawing.Point(3, 3);
+            this.buttonThem.Name = "buttonThem";
+            this.buttonThem.Size = new System.Drawing.Size(210, 32);
+            this.buttonThem.TabIndex = 0;
+            this.buttonThem.Text = "Thêm";
+            this.buttonThem.UseVisualStyleBackColor = true;
+            // 
+            // buttonSua
+            // 
+            this.buttonSua.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonSua.Location = new System.Drawing.Point(219, 3);
+            this.buttonSua.Name = "buttonSua";
+            this.buttonSua.Size = new System.Drawing.Size(210, 32);
+            this.buttonSua.TabIndex = 1;
+            this.buttonSua.Text = "Sửa";
+            this.buttonSua.UseVisualStyleBackColor = true;
+            // 
+            // buttonXoa
+            // 
+            this.buttonXoa.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonXoa.Location = new System.Drawing.Point(435, 3);
+            this.buttonXoa.Name = "buttonXoa";
+            this.buttonXoa.Size = new System.Drawing.Size(211, 32);
+            this.buttonXoa.TabIndex = 2;
+            this.buttonXoa.Text = "Xóa";
+            this.buttonXoa.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableLayoutPanel1.SetColumnSpan(this.dataGridView1, 4);
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(46, 267);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(780, 276);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dateTimePicker.Location = new System.Drawing.Point(570, 135);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(256, 38);
+            this.dateTimePicker.TabIndex = 10;
+            // 
+            // comboBoxTenKhoa
+            // 
+            this.comboBoxTenKhoa.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBoxTenKhoa.FormattingEnabled = true;
+            this.comboBoxTenKhoa.Location = new System.Drawing.Point(177, 47);
+            this.comboBoxTenKhoa.Name = "comboBoxTenKhoa";
+            this.comboBoxTenKhoa.Size = new System.Drawing.Size(256, 37);
+            this.comboBoxTenKhoa.TabIndex = 11;
+            // 
+            // comboBoxTenNganh
+            // 
+            this.comboBoxTenNganh.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBoxTenNganh.FormattingEnabled = true;
+            this.comboBoxTenNganh.Location = new System.Drawing.Point(177, 91);
+            this.comboBoxTenNganh.Name = "comboBoxTenNganh";
+            this.comboBoxTenNganh.Size = new System.Drawing.Size(256, 37);
+            this.comboBoxTenNganh.TabIndex = 12;
+            // 
+            // comboBoxHocKy
+            // 
+            this.comboBoxHocKy.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBoxHocKy.FormattingEnabled = true;
+            this.comboBoxHocKy.Location = new System.Drawing.Point(177, 135);
+            this.comboBoxHocKy.Name = "comboBoxHocKy";
+            this.comboBoxHocKy.Size = new System.Drawing.Size(256, 37);
+            this.comboBoxHocKy.TabIndex = 13;
+            // 
+            // comboBoxHinhThuc
+            // 
+            this.comboBoxHinhThuc.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBoxHinhThuc.FormattingEnabled = true;
+            this.comboBoxHinhThuc.Location = new System.Drawing.Point(177, 179);
+            this.comboBoxHinhThuc.Name = "comboBoxHinhThuc";
+            this.comboBoxHinhThuc.Size = new System.Drawing.Size(256, 37);
+            this.comboBoxHinhThuc.TabIndex = 14;
+            // 
+            // comboBoxMonHoc
+            // 
+            this.comboBoxMonHoc.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBoxMonHoc.FormattingEnabled = true;
+            this.comboBoxMonHoc.Location = new System.Drawing.Point(570, 47);
+            this.comboBoxMonHoc.Name = "comboBoxMonHoc";
+            this.comboBoxMonHoc.Size = new System.Drawing.Size(256, 37);
+            this.comboBoxMonHoc.TabIndex = 15;
+            // 
+            // numericUpDown
+            // 
+            this.numericUpDown.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.numericUpDown.Location = new System.Drawing.Point(570, 91);
+            this.numericUpDown.Name = "numericUpDown";
+            this.numericUpDown.Size = new System.Drawing.Size(256, 38);
+            this.numericUpDown.TabIndex = 16;
+            // 
+            // FormLanThi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 546);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FormLanThi";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormLanThi";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button buttonThem;
+        private System.Windows.Forms.Button buttonSua;
+        private System.Windows.Forms.Button buttonXoa;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.ComboBox comboBoxTenKhoa;
+        private System.Windows.Forms.ComboBox comboBoxTenNganh;
+        private System.Windows.Forms.ComboBox comboBoxHocKy;
+        private System.Windows.Forms.ComboBox comboBoxHinhThuc;
+        private System.Windows.Forms.ComboBox comboBoxMonHoc;
+        private System.Windows.Forms.NumericUpDown numericUpDown;
+    }
+}
